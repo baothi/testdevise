02d1cdb085ade3f78a5862323340b741277af6f2
baothi <baothi246@gmail.com>
2015-08-26 09:41:26 +0700

    Signed-off-by: baothi <baothi246@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
-    :user_name => "xxx@gmail.com",
-    :password => "xxx",
+    :user_name => "baothi246@gmail.com",
+    :password => "NguyenVanThy2005",
     :authentication => 'plain',
     :enable_starttls_auto => true
   }
